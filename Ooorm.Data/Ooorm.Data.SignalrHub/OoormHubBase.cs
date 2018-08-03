3f1c12f10c1fe308d56fa1c1d892c5bbfec14e8c
Kelson Ball <kelson.j.ball@gmail.com>
2018-08-03 15:26:11 -0700

    Add signalr hub project

---
+++ Ooorm.Data/Ooorm.Data.SignalrHub/OoormHubBase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace Ooorm.Data.SignalrHub
+{
+    public class OoormHubBase<T> : Hub where T : IDbItem
+    {
+        protected OoormHubBase(ICrudRepository<T> repo)
+        {
+
+        }
+
+        public async Task Created(T item)
+        {
+
+        }
+    }
+}
